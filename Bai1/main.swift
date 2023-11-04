a5a411206db6ac327515790a2ae54f8b8ce0621d
toan0011 <nmtoan.0011@gmail.com>
2023-11-04 16:48:44 +0700

    Done

---
+++ Bai1/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Bai1
+//
+//  Created by Minh Toan on 04/11/2023.
+//
+
+import Foundation
+
+let keys = [0, 1, 2]
+let values = ["Nam", "Linh", "Mai"]
+let myDictionary = Dictionary(uniqueKeysWithValues: zip(keys, values))
+print(myDictionary)
+
